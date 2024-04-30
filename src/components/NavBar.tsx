96fc76e44b957f30aff5bc4de0c9d5f9af607990
OscarG <oscar.gamez88@gmail.com>
2024-04-30 09:43:13 -0700

    removed default css that came with scaffolding

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,6 @@
 export default function NavBar() {
   return (
-    <nav className="flex h-full items-center justify-center bg-green-300 p-5">
+    <nav className="flex flex-col items-center justify-center bg-green-300 p-5">
       <ul className="flex flex-col justify-between gap-10">
         <li>Home</li>
         <li>About us</li>
