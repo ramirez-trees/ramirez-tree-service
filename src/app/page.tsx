d6da3f9b994c224b3a436bdb15c6b0889532b810
OscarG <oscar.gamez88@gmail.com>
2024-04-30 13:03:15 -0700

    banner for homepage. roughm, needs some extra work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
 export default function HomePage() {
   return (
-    <main className="">
-      <h1>Hello!</h1>
+    <main className="mx-auto my-auto">
+      <div
+        className="flex h-40 w-screen items-center justify-center bg-cover bg-center text-center"
+        style={{
+          backgroundImage: `url('https://www.nps.gov/common/uploads/cropped_image/primary/922495AE-C96F-ED4B-5D1FBBC03B80FB39.jpg?width=1600&quality=90&mode=crop')`,
+        }}
+      >
+        <h1 className="text-3xl text-white">Ramirez Tree Service</h1>
+      </div>
     </main>
   );
 }
