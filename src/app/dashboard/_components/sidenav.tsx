e28289c457252dfae661ddf34fb89eb9f0351fea
OscarG <oscar.gamez88@gmail.com>
2024-05-15 15:36:54 -0700

    adding dashboard nav to layout

--- src/app/dashboard/_components/sidenav.tsx
+++ src/app/dashboard/_components/sidenav.tsx
@@ -1,6 +1,6 @@
 export default function SideNav() {
   return (
-    <ul>
+    <ul className="flex flex-row gap-2">
       <li>Add New Projects</li>
       <li>Projects</li>
       <li>Messages</li>
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import SideNav from "./_components/sidenav";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div>
       <h1>Dashboard</h1>
-      {children}
+      <div>
+        <SideNav />
+        {children}
+      </div>
     </div>
   );
 }
