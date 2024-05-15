2ea161cd24d9ded77094a3c3fe7d1bbb60f13b49
OscarG <oscar.gamez88@gmail.com>
2024-05-15 15:27:54 -0700

    created sidenav for dashboard

---
+++ src/app/dashboard/_components/sidenav.tsx
@@ -0,0 +1,11 @@
+export default function SideNav() {
+  return (
+    <ul>
+      <li>Dashboard</li>
+      <li>Projects</li>
+      <li>Team</li>
+      <li>Messages</li>
+      <li>Reports</li>
+    </ul>
+  );
+}
