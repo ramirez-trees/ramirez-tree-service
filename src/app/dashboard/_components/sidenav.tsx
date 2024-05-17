0fdd2d5fb4f833baca34bdc083e3764bb6b13e51
OscarG <oscar.gamez88@gmail.com>
2024-05-17 12:26:38 -0700

    setting up projects page

--- src/app/dashboard/_components/sidenav.tsx
+++ src/app/dashboard/_components/sidenav.tsx
@@ -1,6 +1,6 @@
 export default function SideNav() {
   return (
-    <ul className="flex flex-col gap-2">
+    <ul className="mx-2 flex flex-col justify-center gap-2">
       <li>Add New Projects</li>
       <li>Projects</li>
       <li>Messages</li>
