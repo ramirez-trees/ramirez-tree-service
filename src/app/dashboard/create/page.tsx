63e5c55f7243ee89c09d5756f9ff319d12d3826b
OscarG <oscar.gamez88@gmail.com>
2024-05-21 13:08:07 -0700

    added redirect logic after upload

--- src/app/dashboard/create/page.tsx
+++ src/app/dashboard/create/page.tsx
@@ -1,18 +1,22 @@
+"use client";
 import { Protect } from "@clerk/nextjs";
-import { auth } from "@clerk/nextjs/server";
 import { UploadButton } from "~/components/uploadthing";
+import { useRouter } from "next/navigation";
 
 export default function CreateProject() {
-  const user = auth();
-  console.log(user);
-
+  const router = useRouter();
   return (
     <Protect
       permission="org:permission:admin"
       fallback={<p>you dont have permission</p>}
     >
       <p>you do have permission</p>
-      <UploadButton endpoint="imageUploader" />
+      <UploadButton
+        endpoint="imageUploader"
+        onClientUploadComplete={() => {
+          router.push("/dashboard");
+        }}
+      />
     </Protect>
   );
 }
