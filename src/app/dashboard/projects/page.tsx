c3499bc8652ba51d65757f4d2b2324797b362c3d
OscarG <oscar.gamez88@gmail.com>
2024-05-24 10:43:26 -0700

    working on delete button

--- src/app/dashboard/projects/page.tsx
+++ src/app/dashboard/projects/page.tsx
@@ -1,3 +1,47 @@
+import { getProjects } from "~/server/queries";
+import Image from "next/image";
+import { Button } from "~/components/ui/button";
+import { deleteProject } from "~/server/queries";
+
 export default function Projects() {
-  return <h1>test projects page</h1>;
+  return (
+    <div>
+      <h1>test projects page</h1>
+      <ProjectImages />
+    </div>
+  );
+}
+
+async function ProjectImages() {
+  const projects = await getProjects();
+  return (
+    <div>
+      {projects.map((project) => (
+        <div key={project.id} className="flex flex-col">
+          <h2 className="mb-2 text-center">{project.name}</h2>
+          <Image
+            src={project.url}
+            alt="project image"
+            className="rounded"
+            width={600}
+            height={400}
+          />
+          <form
+            action={async () => {
+              "use server";
+              await deleteProject(project.id);
+            }}
+          >
+            <Button
+              type="submit"
+              variant="destructive"
+              className="rounded bg-red-400"
+            >
+              delete
+            </Button>
+          </form>
+        </div>
+      ))}
+    </div>
+  );
 }
--- src/server/queries.ts
+++ src/server/queries.ts
@@ -1,9 +1,27 @@
 // import "use-server";
 import { db } from "./db";
+import { auth } from "@clerk/nextjs/server";
+import { projects } from "./db/schema";
+import { eq } from "drizzle-orm";
 
 export async function getProjects() {
   const projects = await db.query.projects.findMany({
     orderBy: (model, { desc }) => desc(model.createdAt),
   });
   return projects;
+}
+
+export async function deleteProject(id: number) {
+  const user = auth();
+
+  if (!user.userId) throw new Error("Unauthorized");
+
+  const image = await db.query.projects.findFirst({
+    where: (model, { eq }) => eq(model.id, id),
+  });
+
+  if (!image) throw new Error("Not found");
+
+  await db.delete(projects).where(eq(projects.id, id));
+  return image;
 }
