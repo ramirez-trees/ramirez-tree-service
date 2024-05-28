1062b8c62ac0e6daf58d0ec5ba4f6000da2a6f51
OscarG <oscar.gamez88@gmail.com>
2024-05-28 11:01:41 -0700

    button styling for dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,6 +5,7 @@ import {
   SignOutButton,
 } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
+import { Button } from "~/components/ui/button";
 
 export default function DashBoard() {
   const user = auth();
@@ -14,14 +15,18 @@ export default function DashBoard() {
       <h1>Dashboard</h1>
       <SignedOut>
         <p>Please sign in</p>
-        <SignInButton />
+        <Button className="rounded bg-blue-400">
+          <SignInButton />
+        </Button>
       </SignedOut>
       <SignedIn>
         <p>you are signed in</p>
         <p>this is your userId: {user.userId}</p>
         <p>this is your org id: {user.orgId}</p>
         <p>this is your user roles: {user.orgRole}</p>
-        <SignOutButton />
+        <Button className="rounded bg-red-400">
+          <SignOutButton />
+        </Button>
       </SignedIn>
     </div>
   );
