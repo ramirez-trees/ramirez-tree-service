bd3ccfe18d31da85e91a51280162a7855779b0d5
OscarG <oscar.gamez88@gmail.com>
2024-05-30 11:40:39 -0700

    modal and image page working correctly

--- src/app/@modal/(.)img/[id]/page.tsx
+++ src/app/@modal/(.)img/[id]/page.tsx
@@ -1,16 +1,26 @@
-// import { getProject } from "~/server/queries";
-// import { Modal } from "./modal";
-// import Image from "next/image";
+import { getProject } from "~/server/queries";
+import { Modal } from "./modal";
+import Image from "next/image";
 
 export default async function PhotoModal({
   params: { id: photoId },
 }: {
   params: { id: string };
 }) {
-  // const idAsNumber = Number(photoId);
-  // if (Number.isNaN(idAsNumber)) throw new Error("Invalid ID");
+  const idAsNumber = Number(photoId);
+  if (Number.isNaN(idAsNumber)) throw new Error("Invalid ID");
 
-  // const image = await getProject(idAsNumber);
+  const image = await getProject(idAsNumber);
 
-  return <div>{photoId}</div>;
+  return (
+    <Modal>
+      <Image
+        src={image.url}
+        alt="landscaping work picture"
+        className="mx-auto mt-10"
+        width={800}
+        height={600}
+      />
+    </Modal>
+  );
 }
--- src/app/img/[id]/page.tsx
+++ src/app/img/[id]/page.tsx
@@ -1,16 +1,25 @@
-// import { getProject } from "~/server/queries";
-// import { Modal } from "./modal";
-// import Image from "next/image";
+import { getProject } from "~/server/queries";
+import Image from "next/image";
 
 export default async function PhotoModal({
   params: { id: photoId },
 }: {
   params: { id: string };
 }) {
-  // const idAsNumber = Number(photoId);
-  // if (Number.isNaN(idAsNumber)) throw new Error("Invalid ID");
+  const idAsNumber = Number(photoId);
+  if (Number.isNaN(idAsNumber)) throw new Error("Invalid ID");
 
-  // const image = await getProject(idAsNumber);
+  const image = await getProject(idAsNumber);
 
-  return <div>{photoId}</div>;
+  return (
+    <div className="flex items-center justify-center">
+      <Image
+        src={image.url}
+        alt="example landscaping work"
+        className="mt-10"
+        height={600}
+        width={600}
+      />
+    </div>
+  );
 }
