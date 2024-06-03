557163e7a94e96b71590959fd77737da736abeec
OscarG <oscar.gamez88@gmail.com>
2024-06-03 14:50:08 -0700

    embeding youtube videos

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -6,8 +6,18 @@ export const dynamic = "force-dynamic";
 
 export default function Projects() {
   return (
-    <div>
+    <div className="container">
       <h1 className="my-3 text-center text-3xl">Previous Work</h1>
+      <iframe
+        width="439"
+        height="781"
+        src="https://www.youtube.com/embed/YW-PhEHNe0w"
+        title="Gravel path plus Patio"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowfullscreen
+      ></iframe>
       <Images />
     </div>
   );
