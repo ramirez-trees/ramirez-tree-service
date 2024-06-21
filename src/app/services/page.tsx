572282972307b4e2076a5e41597e9b9cb8865dca
OscarG <oscar.gamez88@gmail.com>
2024-06-21 13:27:37 -0700

    fleshed out services page

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,12 +1,10 @@
 import { Separator } from "~/components/ui/separator";
+import Link from "next/link";
 
 export default function Services() {
-  // might move the services into a variable and map it to make it easier to tailwind one tag
   return (
     <>
       <div className="mt-5 flex flex-col items-center justify-center gap-5">
-        <h1 className="text-center text-4xl">Services</h1>
-        <p>blurb about the services that we offer</p>
         <div className="mx-auto flex flex-col">
           <p className="text-3xl">List of services</p>
           <Separator className="my-4" style={{ backgroundColor: "black" }} />
@@ -23,6 +21,13 @@ export default function Services() {
             <li>and more!</li>
           </ul>
         </div>
+        <p className="text-center text-xl">
+          Need something else done you don&apos;t see here?{" "}
+          <Link className="text-blue-700 underline" href={"/contact_us"}>
+            Contact us
+          </Link>{" "}
+          and let us know how we can help.
+        </p>
       </div>
     </>
   );
