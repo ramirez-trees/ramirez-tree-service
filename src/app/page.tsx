977c8dcf1cbe87c2fe163dd98dc1334f00ac0c74
OscarG <oscar.gamez88@gmail.com>
2024-06-21 13:15:22 -0700

    main page blurb done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,10 @@ export default function HomePage() {
         <Separator className="my-4" style={{ backgroundColor: "black" }} />
         <p className="text-xl md:w-1/2">
           Ramirez Tree Service has been servicing Marin and Sonoma country for
-          over 30 years.
+          over 30 years. Our team of skilled professionals are licensed and
+          equipped to handle a wide range of projects. We pride ourselves
+          ourselves on delivering high quality workmanship and customer service
+          to gaurentee your landscaping needs are met.
         </p>
         <Image
           src="https://i.imgur.com/3q73nnP.jpeg"
