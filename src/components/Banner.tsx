58d7780bdda1939179fb8106cff6ebd3be616c8a
OscarG <oscar.gamez88@gmail.com>
2024-06-21 20:58:51 -0700

    added landscaping to business name

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -6,7 +6,9 @@ export default function Banner() {
         backgroundImage: `url('https://www.nps.gov/common/uploads/cropped_image/primary/922495AE-C96F-ED4B-5D1FBBC03B80FB39.jpg?width=1600&quality=90&mode=crop')`,
       }}
     >
-      <h1 className="text-3xl text-white">Ramirez Tree Service</h1>
+      <h1 className="text-3xl text-white">
+        Ramirez Tree Service and Landscaping
+      </h1>
     </div>
   );
 }
