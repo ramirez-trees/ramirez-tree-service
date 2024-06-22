acd8c3036c515a37ac40dcc96adffb41d1aa837e
OscarG <oscar.gamez88@gmail.com>
2024-06-21 20:45:12 -0700

    fixed typo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,9 +10,9 @@ export default function HomePage() {
         <p className="text-xl md:w-1/2">
           Ramirez Tree Service has been servicing Marin and Sonoma country for
           over 30 years. Our team of skilled professionals are licensed and
-          equipped to handle a wide range of projects. We pride ourselves
-          ourselves on delivering high quality workmanship and customer service
-          to gaurentee your landscaping needs are met.
+          equipped to handle a wide range of projects. We pride ourselves on
+          delivering high quality workmanship and customer service to gaurentee
+          your landscaping needs are met.
         </p>
         <Image
           src="https://i.imgur.com/3q73nnP.jpeg"
