6061039b10a05c906d9abc67182ff69b3c1ff46c
OscarG <oscar.gamez88@gmail.com>
2024-06-24 12:45:04 -0700

    cleaned up commented out code

--- src/app/@modal/(.)img/[id]/modal.tsx
+++ src/app/@modal/(.)img/[id]/modal.tsx
@@ -26,7 +26,6 @@ export function Modal({ children }: { children: React.ReactNode }) {
       onClick={onDismiss}
     >
       {children}
-      {/* <button onClick={onDismiss} className="close-button" /> */}
     </dialog>,
     document.getElementById("modal-root")!,
   );
